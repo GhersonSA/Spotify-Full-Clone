6693b02aa7021b88b79ee4d10c81bf8cd530c4bd
Gherson <Gherson2002@outlook.com>
2025-09-11 23:09:17 +0200

    Data fix

--- src/data/mobile/cardMobile.js
+++ src/data/mobile/cardMobile.js
@@ -1,42 +1,42 @@
 export const recientes = [
-    { img: "/public/images/Maluma.webp", nombre: "Maluma"},
-    { img: "/public/images/Feid.webp", nombre: "Feid"},
-    { img: "/public/images/Ferxxocalipsis.webp", nombre: "LUNA"},
-    { img: "/public/images/Addicted.webp", nombre: "Addicted"},
+    { img: "/images/Maluma.webp", nombre: "Maluma"},
+    { img: "/images/Feid.webp", nombre: "Feid"},
+    { img: "/images/Ferxxocalipsis.webp", nombre: "LUNA"},
+    { img: "/images/Addicted.webp", nombre: "Addicted"},
 ]
 
 export const recomendados = [
-    { img: "/public/images/TopGlobal.webp", nombre: "Top 50: Global"},
-    { img: "/public/images/TopSpain.webp", nombre: "Top 50: España"},
-    { img: "/public/images/TopEcuador.webp", nombre: "Top 50: Ecuador"},
-    { img: "/public/images/TopUSA.webp", nombre: "Top 50: USA"},
-    { img: "/public/images/TopColombia.webp", nombre: "Top 50: Colombia"},
-    { img: "/public/images/TopArgentina.webp", nombre: "Top 50: Argentina"},
-    { img: "/public/images/TopNicaragua.webp", nombre: "Top 50: Nicaragua"},
-    { img: "/public/images/TopUK.webp", nombre: "Top 50: UK"},
-    { img: "/public/images/TopMexico.webp", nombre: "Top 50: México"}
+    { img: "/images/TopGlobal.webp", nombre: "Top 50: Global"},
+    { img: "/images/TopSpain.webp", nombre: "Top 50: España"},
+    { img: "/images/TopEcuador.webp", nombre: "Top 50: Ecuador"},
+    { img: "/images/TopUSA.webp", nombre: "Top 50: USA"},
+    { img: "/images/TopColombia.webp", nombre: "Top 50: Colombia"},
+    { img: "/images/TopArgentina.webp", nombre: "Top 50: Argentina"},
+    { img: "/images/TopNicaragua.webp", nombre: "Top 50: Nicaragua"},
+    { img: "/images/TopUK.webp", nombre: "Top 50: UK"},
+    { img: "/images/TopMexico.webp", nombre: "Top 50: México"}
 ]
 
 export const favoritos = [
-    { img: "/public/images/Maluma.webp", nombre: "Maluma"},
-    { img: "/public/images/Feid.webp", nombre: "Feid"},
-    { img: "/public/images/DuaLipa.webp", nombre: "Dua Lipa"},
-    { img: "/public/images/Marshmello.webp", nombre: "Marshmello"},
-    { img: "/public/images/Panda-Eyes.webp", nombre: "Panda Eyes"}
+    { img: "/images/Maluma.webp", nombre: "Maluma"},
+    { img: "/images/Feid.webp", nombre: "Feid"},
+    { img: "/images/DuaLipa.webp", nombre: "Dua Lipa"},
+    { img: "/images/Marshmello.webp", nombre: "Marshmello"},
+    { img: "/images/Panda-Eyes.webp", nombre: "Panda Eyes"}
 ]
 
 export const escuchado = [
-    { img: "/public/images/Verano-Rosa.webp", nombre: "Verano Rosa"},
-    { img: "/public/images/Pretty-Boy-Dirty-Boy.webp", nombre: "Pretty Boy, Dirty Boy"},
-    { img: "/public/images/Karol-G.webp", nombre: "Karol G"},
-    { img: "/public/images/Marshmello.webp", nombre: "Marshmello"}
+    { img: "/images/Verano-Rosa.webp", nombre: "Verano Rosa"},
+    { img: "/images/Pretty-Boy-Dirty-Boy.webp", nombre: "Pretty Boy, Dirty Boy"},
+    { img: "/images/Karol-G.webp", nombre: "Karol G"},
+    { img: "/images/Marshmello.webp", nombre: "Marshmello"}
 ]
 
 export const populares = [
-    { img: "/public/images/Don-Juan.webp", nombre: "Don Juan"},
-    { img: "/public/images/Feliz-Cumpleaños-Ferxxo.webp", nombre: "Feliz Cumpleaños Ferxxo"},
-    { img: "/public/images/Future-Nostalgia.webp", nombre: "Future Nostalgia"},
-    { img: "/public/images/Shockwave.webp", nombre: "Shockwave"},
-    { img: "/public/images/Ferxxocalipsis.webp", nombre: "FERXXOCALIPSIS"},
-    { img: "/public/images/FAME.webp", nombre: "F.A.M.E"}
+    { img: "/images/Don-Juan.webp", nombre: "Don Juan"},
+    { img: "/images/Feliz-Cumpleaños-Ferxxo.webp", nombre: "Feliz Cumpleaños Ferxxo"},
+    { img: "/images/Future-Nostalgia.webp", nombre: "Future Nostalgia"},
+    { img: "/images/Shockwave.webp", nombre: "Shockwave"},
+    { img: "/images/Ferxxocalipsis.webp", nombre: "FERXXOCALIPSIS"},
+    { img: "/images/FAME.webp", nombre: "F.A.M.E"}
 ]
